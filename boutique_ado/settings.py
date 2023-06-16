28e62232dca8abbb8d5dfea7c1eb0a912e3cf9aa
oconnorian3 <oconnorian3@gmail.com>
2023-06-16 20:10:16 +0000

    webh

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-oconnorian3-boutiqueado-ahbeo1k01e8.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-oconnorian3-boutiqueado-ahbeo1k01e8.ws-eu100.gitpod.io']
 
 
 
@@ -173,3 +173,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
