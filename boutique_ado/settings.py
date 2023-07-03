9efa690a7898ed26239ed1243e47ac50ae96ccde
oconnorian3 <oconnorian3@gmail.com>
2023-07-03 08:14:20 +0000

    Deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique86.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique86-34df17983717.herokuapp.com', 'localhost']
 
 
 
