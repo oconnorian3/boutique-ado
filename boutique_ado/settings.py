2dffebf4a0d0714ef1aa9d95f2350b0beba29398
oconnorian3 <oconnorian3@gmail.com>
2023-07-03 10:12:44 +0000

    a

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -171,6 +171,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
